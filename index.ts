06d13f12f3066ebe12bf62d18eee38b44413c641
anas <anasahmedshaikh2007@gmail.com>
2024-03-21 09:38:36 +0500

    updating the code to add more functionality

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@
 import inquirer from "inquirer";
 
 const answer= await inquirer.prompt([
-  {message: "Enter your first number", type: "number", name: "firstNumber" },
-  {message: "Enter your second number", type: "number", name: "secondNumber" },
+  {message: "Enter first number", type: "number", name: "firstNumber" },
+  {message: "Enter second number", type: "number", name: "secondNumber" },
   {
     message: "select one of these operator to perform operation",
     type: "list",
@@ -30,3 +30,4 @@ else if (answer.operator === "Division"){
 else {
     console.log("select a valid operator");
 }
+console.log("THE END");
